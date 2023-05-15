d06f55857de07b105e4eb4db18b8605b91da2061
Divya R <kwrdivyaraj100@gmail.com>
2023-05-15 20:16:14 +0530

    show/hiding the sidebar with a button

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const arrows = document.querySelector('.left-content');
+
+arrows.addEventListener('click', e => {
+   
+     const menuClass = e.target.classList;
+     //  console.log(menuClass);
+    
+     const container = document.querySelector('.app'),
+           leftArrow = document.querySelector('.fa-arrow-left'),
+           rightArrow = document.querySelector('.fa-arrow-right');
+
+    
+     if(menuClass.contains('fa-arrow-left')){
+        
+          container.classList.add('no-menu');
+          
+          e.target.style.display = 'none';
+       
+          rightArrow.style.display = 'block';
+     } else if(menuClass.contains('fa-arrow-right')) {
+          
+          container.classList.remove('no-menu');
+         
+          e.target.style.display = 'none';
+          
+          leftArrow.style.display = 'block';
+     }
+})
